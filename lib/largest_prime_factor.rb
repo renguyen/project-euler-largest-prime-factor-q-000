67738a1f4174e26482c943aef74f13ad601fd70b
renguyen <reginanguyen01@gmail.com>
2016-03-30 17:25:42 -0700

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,14 @@
 # Enter your procedural solution here!
+def largest_prime_factor(num)
+  factors = Array.new
+  divisor = 2
+  while (num > 1)
+    while (num % divisor == 0)
+      factors << divisor
+      num /= divisor
+    end
+    divisor += 1
+  end
+
+  return factors.max
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,22 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+
+  def initialize(num)
+    @num = num
+  end
+
+  def number
+    factors = Array.new
+    divisor = 2
+    while (@num > 1)
+      while (@num % divisor == 0)
+        factors << divisor
+        @num /= divisor
+      end
+      divisor += 1
+    end
+
+    return factors.max
+  end
+
+end
